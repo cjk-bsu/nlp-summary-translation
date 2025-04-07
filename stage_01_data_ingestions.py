2143eed8a46cc7741f22eb83ce066d3b2ae674e6
LeviRecla <levirecla@u.boisestate.edu>
2025-04-06 19:19:18 -0600

    data ingestion stage (should go in src/pipeline directory)

---
+++ stage_01_data_ingestions.py
@@ -0,0 +1,15 @@
+## should go in src/pipeline
+from testSummarizer.config.configuration import ConfigurationManager
+from testSummarizer.components.data_ingestion import DataIngestion
+from testSummarizer.logging import logger
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
