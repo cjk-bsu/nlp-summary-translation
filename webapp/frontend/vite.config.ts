f692f6fa62e35d7da8848b96a1e6d4bcda6c3bb0
Carson <123137691+cjk-bsu@users.noreply.github.com>
2025-04-16 21:50:16 +0000

    Implemented starter proxy to backend

--- webapp/frontend/vite.config.ts
+++ webapp/frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://urban-couscous-j6gx7rg6rqqcq7w9-3000.app.github.dev',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  }
 })
